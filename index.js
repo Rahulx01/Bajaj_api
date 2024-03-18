98d1058915e60c72d6daa5b4545696057bc485f3
Rahul Goyal <101088902+Rahulx01@users.noreply.github.com>
2024-03-18 13:34:24 +0530

    final changes

--- index.js
+++ index.js
@@ -12,36 +12,41 @@ app.get("/", (req, res) => {
 });
 
 app.post("/bfhl", (req, res) => {
-  const {data} = req.body.res;
-  const alphabets = [];
-  const even_numbers = [];
-  const odd_numbers = [];
+  try {
+    const { data } = req.body;
+    const alphabets = [];
+    const even_numbers = [];
+    const odd_numbers = [];
 
-  for (const char of data) {
-    if (char.length === 1 && char.match(/[a-z]/i)) {
-      alphabets.push(char.toUpperCase());
-    } else {
-      const number = parseInt(char, 10);
-      if (!isNaN(number)) { 
-        if (number % 2 === 0) {
-          even_numbers.push(number);
-        } else {
-          odd_numbers.push(number);
+    for (const char of data) {
+      if (char.length === 1 && char.match(/[a-z]/i)) {
+        alphabets.push(char.toUpperCase());
+      } else {
+        const number = parseInt(char, 10);
+        if (!isNaN(number)) {
+          if (number % 2 === 0) {
+            even_numbers.push(number);
+          } else {
+            odd_numbers.push(number);
+          }
         }
       }
     }
-  }
-  const response = {
-    is_success: true,
-    user_id: "rahul_goyal_01092003",
-    email: "rahul1120.be21@chitkara.edu.in",
-    roll_number: "2110991120",
-    odd_numbers: [],
-    even_numbers: [],
-    alphabets: [],
-  };
+    const response = {
+      is_success: true,
+      user_id: "rahul_goyal_01092003",
+      email: "rahul1120.be21@chitkara.edu.in",
+      roll_number: "2110991120",
+      odd_numbers,
+      even_numbers,
+      alphabets,
+    };
 
-  res.status(200).json(response);
+    res.status(200).json(response);
+  }
+  catch (err) {
+    res.status(400).json({ is_success: false, error: err.message });
+  }
 });
 
 app.listen(PORT, () => {
